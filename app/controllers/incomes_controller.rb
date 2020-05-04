91d3c08612cd1fe0a218998ab75a665b39179252
yanbaru917 <ttaakkeemsss@gmail.com>
2020-05-04 14:56:35 +0900

    new-income-controller

---
+++ app/controllers/incomes_controller.rb
@@ -0,0 +1,33 @@
+class IncomesController < ApplicationController
+
+  def index
+    @incomes = Income.order(created_at: :asc)
+  end
+
+  def new
+    @income = Income.new
+  end
+
+  def create
+    Income.create(income_params)
+    redirect_to root_path
+  end
+
+  def update
+    
+  end
+
+  def edit
+    @income = Income.find(params[:id])
+  end
+
+  def destroy
+    @income = Income.find(params[:id])
+    @income.destroy
+  end
+
+  private
+  def income_params
+    params.require(:income).permit(:income_name, :income_amount)
+  end
+end
