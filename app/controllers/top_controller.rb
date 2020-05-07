fc727cf58bb96af43a48a68f143d66b128f4bc2f
yanbaru917 <ttaakkeemsss@gmail.com>
2020-05-07 14:27:25 +0900

    new-balance_payment

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,5 +1,9 @@
 class TopController < ApplicationController
   def index
-    
+    @incomes = Income.order(income_date: :asc)
+    @total_income_amount = @incomes.sum(:income_amount)
+    @expenses = Expense.order(expense_date: :asc)
+    @total_expense_amount = @expenses.sum(:expense_amount)
+    @balance_payment = @total_income_amount - @total_expense_amount
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   root "top#index"
+  get '/incomes', to: 'incomes#index'
+  get '/tops', to: 'incomes#index'
+  get '/expenses', to: 'expenses#index'
+  get '/tops', to: 'expenses#index'
+
   namespace :incomes do
     resources :searches, only: :index
   end
